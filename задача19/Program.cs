f3e444773cbb1cd92da5e6b83e4fae7cc143437a
29Tatiana <amiravob@mail.ru>
2022-09-12 10:04:31 +0300

    задача19ред

--- задача19/Program.cs
+++ задача19/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine()!;
+
+
+bool isPolindrom = Polindrom(number);
+
+string txt = isPolindrom == true? "палиндром" : "Не палиндром";
+Console.WriteLine(txt);
+
+bool Polindrom(string number)
+{
+for (int i = 0; i < number.Length / 2; i++)
+{
+if (number[i] != number[number.Length - i - 1])
+return false;
+}
+return true;
+}
